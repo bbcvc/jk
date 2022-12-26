40c211e47a4738aa0b414eded09d1859f843ca60
chenglong <1103787680@qq.com>
2022-12-26 13:08:53 +0800

    chore: format

--- script/index.mjs
+++ script/index.mjs
@@ -3,40 +3,39 @@ import puppeteer from 'puppeteer'
 import fs from 'node:fs'
 
 (async () => {
-    // 启动浏览器
-    const browser = await puppeteer.launch({
-      headless: true, // 默认是无头模式，这里为了示范所以使用正常模式
-    })
+  // 启动浏览器
+  const browser = await puppeteer.launch({
+    headless: true, // 默认是无头模式，这里为了示范所以使用正常模式
+  })
 
-    // 控制浏览器打开新标签页面
-    const page = await browser.newPage()
-    await page.setViewport({
-	    width: 2560,
-	    height: 3000,
-	  });
-    // 在新标签中打开要爬取的网页
-    await page.goto('https://jk.rs')
-    await page.waitForSelector('#masonry')
-    await page.mouse.move(0, 2000)
+  // 控制浏览器打开新标签页面
+  const page = await browser.newPage()
+  await page.setViewport({
+    width: 2560,
+    height: 3000,
+  });
+  // 在新标签中打开要爬取的网页
+  await page.goto('https://jk.rs')
+  await page.waitForSelector('#masonry')
 
-    // 使用evaluate方法在浏览器中执行传入函数（完全的浏览器环境，所以函数内可以直接使用window、document等所有对象和方法）
-    let data = await page.evaluate(() => {
-        let elements = [...document.querySelectorAll('img')]
+  // 使用evaluate方法在浏览器中执行传入函数（完全的浏览器环境，所以函数内可以直接使用window、document等所有对象和方法）
+  let data = await page.evaluate(() => {
+    let elements = [...document.querySelectorAll('img')]
 
-        let data = []
-        for (let i=0; i< elements.length - 1; i++) {
-        	//获取新闻的标签
-            let title = elements[i].alt;
-            //获取新闻的链接地址
-            let url = elements[i]?.getAttribute('src');
-            //将获取到的标题和链接地址添加到数组中
-            data.push({
-                title, 
-                url
-            })
-          }
-        return data.filter(img => !img.url?.endsWith('lazy.svg'))
-    })
-    fs.writeFileSync('../doc/res.json', JSON.stringify(data, null, 2))
-    await browser.close()
+    let data = []
+    for (let i = 0; i < elements.length - 1; i++) {
+      //获取新闻的标签
+      let title = elements[i].alt;
+      //获取新闻的链接地址
+      let url = elements[i]?.getAttribute('src');
+      //将获取到的标题和链接地址添加到数组中
+      data.push({
+        title,
+        url
+      })
+    }
+    return data.filter(img => !img.url?.endsWith('lazy.svg'))
+  })
+  fs.writeFileSync('../doc/res.json', JSON.stringify(data, null, 2))
+  await browser.close()
 })()
