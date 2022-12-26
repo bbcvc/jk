ddebeec55612c173671570418dc8c854acfc320d
chenglong.xing <chenglong.xing@cyclone-robotics.com>
2022-12-26 10:52:46 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import list from '../../doc/res.json'
+
+defineProps<{ msg: string }>()
+
+const count = ref(list)
+</script>
+
+<template>
+  <div class="content">
+    <div v-for="i of count" class="content-list">
+      <!-- <p>{{ i.title }}</p> -->
+      <img :src='i.url' alt="">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.content {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 1200px;
+  margin: auto;
+}
+.content-list {
+  width: 33%;
+}
+.content-list > img {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+</style>
